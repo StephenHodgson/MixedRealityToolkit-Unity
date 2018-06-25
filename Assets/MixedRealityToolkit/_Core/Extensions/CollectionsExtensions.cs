34130eed0dbf2e7ff1e77c9adc40386770168675
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2018-06-25 15:32:07 +0100

    Fixed fromatting

--- Assets/MixedRealityToolkit/_Core/Extensions/CollectionsExtensions.cs
+++ Assets/MixedRealityToolkit/_Core/Extensions/CollectionsExtensions.cs
@@ -160,15 +160,10 @@ namespace Microsoft.MixedReality.Toolkit.Internal.Extensions
 
 
         /// <summary>
-
         /// Overload extension to enable getting of an InteractionDefinition of a specific type
-
         /// </summary>
-
         /// <param name="input">The InteractionDefinition array reference</param>
-
         /// <param name="key">The specific DeviceInputType value to query</param>
-
         public static MixedRealityInteractionMapping GetInteractionbyType(this MixedRealityInteractionMapping[] input, DeviceInputType key)
         {
             for (int i = 0; i < input.Length; i++)
